edd571493195e01554808a80afd4f9fcec2b19d9
Aliaksei Yafremau <447654972g@gmail.com>
2025-02-03 03:27:22 +0000

    Add dot, space, lenght, symbols validation

--- tests/test_validate_email.py
+++ tests/test_validate_email.py
@@ -2,6 +2,9 @@ from validat.validators import validate_email
 
 
 def test_at_sign():
+    """
+    Check for the number of @ signs
+    """
     multiple_at_email = "@testemail@domain.com"
     zero_at_email = "testemaildomain.com"
     one_at_email = "testemail@domain.com"
@@ -11,9 +14,69 @@ def test_at_sign():
     assert validate_email(one_at_email) == True
 
 
-def test_dot_sign():
-    double_dot_email = "test..example@domain.com"
-    one_dot_email = "test.example@domain.com"
+def test_double_dot():
+    """
+    Check for two dots in a row
+    """
+    double_dot_in_username_in_the_start = "..testexample@domain.com"
+    double_dot_in_username_in_the_middle = "test..example@domain.com"
+    double_dot_in_username_in_the_end = "testexample..@domain.com"
+    
+    double_dot_in_domain_in_the_start = "testexample@..domain.com"
+    double_dot_in_domain_in_the_middle = "testexample@doma..in.com"
+    double_dot_in_domain_in_the_end = "testexample@domain.com.."
 
-    assert validate_email(double_dot_email) == False
-    assert validate_email(one_dot_email) == True
+    assert validate_email(double_dot_in_username_in_the_start) == False
+    assert validate_email(double_dot_in_username_in_the_middle) == False
+    assert validate_email(double_dot_in_username_in_the_end) == False
+    
+    assert validate_email(double_dot_in_domain_in_the_start) == False
+    assert validate_email(double_dot_in_domain_in_the_middle) == False
+    assert validate_email(double_dot_in_domain_in_the_end) == False
+
+
+def test_single_dot_position():
+    """
+    Check for point position
+    """
+    dot_in_username_in_the_start = ".testexample@domain.com"
+    dot_in_username_in_the_middle = "test.example@domain.com"
+    dot_in_username_in_the_end = "testexample.@domain.com"
+
+    dot_in_domain_in_the_start = "testexample@.domain.com"
+    dot_in_domain_in_the_middle = "testexample@doma.in.com"
+    dot_in_domain_in_the_end = "testexample@domain.com."
+
+    assert validate_email(dot_in_username_in_the_start) == False
+    assert validate_email(dot_in_username_in_the_middle) == True
+    assert validate_email(dot_in_username_in_the_end) == False
+
+    assert validate_email(dot_in_domain_in_the_start) == False
+    assert validate_email(dot_in_domain_in_the_middle) == True
+    assert validate_email(dot_in_domain_in_the_end) == False
+
+
+def test_incomplete_email():
+    """
+    Check for complete email adress
+    """
+    email_without_username = "@domain.com"
+    email_without_domain = "testexample@"
+
+    assert validate_email(email_without_username) == False
+    assert validate_email(email_without_domain) == False
+
+
+def test_spaces():
+    """
+    Check for spaces
+    """
+    email_with_space_in_username = "test example@domain.com"
+    email_with_space_in_domain = "testexample@dom ain.com"
+    email_with_space_in_the_start = " testexample@domain.com"
+    email_with_space_in_the_end = "testexample@domain.com "
+
+    assert validate_email(email_with_space_in_username) == False
+    assert validate_email(email_with_space_in_domain) == False
+    assert validate_email(email_with_space_in_the_start) == False
+    assert validate_email(email_with_space_in_the_end) == False
--- validat/validators.py
+++ validat/validators.py
@@ -1,5 +1,44 @@
+banned = {'!', '#', '$', '%', '^', '&', '*', '(', ')'}
+
 def validate_email(email: str) -> bool:
-    at_sign_count = email.count("@")
+    at_sign_count = email.count('@')
+    
+
     if at_sign_count != 1:
         return False
+    
+    if len(email) > 254:
+        return False
+    
+    if banned.intersection(set(email)):
+        return False
+
+    if '..' in email:
+        return False
+    
+    if ' ' in email:
+        return False
+    
+    at_index = email.find('@')
+    username = email[:at_index]
+    domain = email[at_index + 1:]
+
+    if not username:
+        return False
+    
+    if "." == username[0]:
+        return False
+    
+    if "." == username[-1]:
+        return False
+    
+    if not domain:
+        return False
+    
+    if "." == domain[0]:
+        return False
+    
+    if "." == domain[-1]:
+        return False
+
     return True
