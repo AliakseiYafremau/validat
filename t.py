074f6244ac0e551af32c3a2eb8fcd6584a07a841
Aliaksei Yafremau <aleksey.yafremau@gmail.com>
2025-04-17 09:59:10 +0100

    Lint and format

--- t.py
+++ t.py
@@ -1,13 +1,13 @@
 import validat
 
 try:
-   correct_email = "username@example.com"
-   is_valid = validat.validate_email(correct_email)
+    correct_email = "username@example.com"
+    is_valid = validat.validate_email(correct_email)
 except validat.EmailValidationError as e:
-   print(f"Email validation error: {e.message}")
+    print(f"Email validation error: {e.message}")
 
 try:
-   wrong_email = "username@@example.com"
-   is_valid = validat.validate_email(wrong_email)
+    wrong_email = "username@@example.com"
+    is_valid = validat.validate_email(wrong_email)
 except validat.EmailValidationError as e:
-   print(f"Email validation error: {e.message}")
+    print(f"Email validation error: {e.message}")
